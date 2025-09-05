945580614a1c2702598b9fb73bf5b108bd11ddba
Jason <therawan.jason@gmail.com>
2025-09-05 22:50:05 +0700

    Finish 06-11 exercises

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,4 +1,14 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr) {
+    const args = Array.prototype.slice.call(arguments, 1);
+    for(let num of args){
+        for(let i = 0; i < arr.length; i++) {
+            if(arr[i] === num) {
+                arr.splice(i, 1);
+                i--;
+            }
+        }
+    }
+    return arr;
 };
 
 // Do not edit below this line
